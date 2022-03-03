2ec060450e1d1d7235de16aa76c9d2869a1d4c74
gladyswambura <gladyswahito7@gmail.com>
2022-03-03 08:48:20 +0300

    Creates a calculator that prompts the user to key in numbers twice and gives an answer

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// addition
+var add=function(number1, number2){
+    return number1 + number2;
+}
+var number1=parseInt(prompt("Enter a number"));
+var number2=parseInt(prompt("Enter another number"));
+var result=add (number1, number2);
+alert(result);
+
+// subtraction
+var subtract=function(number1, number2){
+    return number1-number2;
+}
+var number1 = parseInt(prompt("Enter  number"));
+var number2 = parseInt(prompt("Enter another number"));
+var result = subtract(number1, number2);
+alert(result);
+
+// multiplication
+var multiply=function(number1, number2){
+    return number1*number2;
+}
+var number1 = parseInt(prompt("Enter  number"));
+var number2 = parseInt(prompt("Enter another number"));
+var result = multiply(number1, number2);
+alert(result);
+
